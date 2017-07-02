9c62d2e649e342bd43d4505bf4c612145557e67e
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-01 22:05:34 -0500

    Update vampire tracking program

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,14 +2,52 @@ puts "What is your name?"
 
 name = gets.chomp
 
-puts "How old are you? What year were you born?"
+  if name == "Drake Cula"
+    vamp_names_are_obvi = true
+  elsif name == "Tu Fang"
+    vamp_names_are_obvi = true
+  else
+    vamp_names_are_obvi = false
+  end
+
+puts "How old are you?"
 
 age = gets.chomp
+
+  if age >= "100"
+    vamps_are_old = true
+  else age <= "100"
+    vamps_are_old = false
+  end
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 
 bread = gets.chomp
 
+  if bread == "No"
+    vamps_hate_garlic = true
+  else
+    vamps_hate_garlic = false
+  end
+
 puts "Would you like to enroll in the company's health insurance?"
 
 health = gets.chomp
+
+  if health == "No"
+    vamps_donut_need_insurance = true
+  else
+    vamps_donut_need_insurance = false
+  end
+
+if !(vamps_are_old && vamps_hate_garlic && vamps_donut_need_insurance)
+  puts "Probably not a vampire"
+elsif vamps_are_old && vamps_hate_garlic || vamps_donut_need_insurance
+  puts "Probably a vampire"
+elsif vamps_are_old && vamps_hate_garlic && vamps_donut_need_insurance
+  puts "Almost certainly a vampire"
+elsif vamp_names_are_obvi
+  puts "Definitely a vampire"
+else
+  puts "Results inconclusive"
+end

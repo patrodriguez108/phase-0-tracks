734bc3e1f78146a1ba4299b47995ebab378e9cdc
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-02 13:56:39 -0500

    Update ham questionnaire to print to console

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,16 +2,20 @@ puts "What is the hamster's name?"
 
 name = gets.chomp
 
+p "Aww! #{name} is a cute name :)"
 
 puts "What is the volume of hamster squeakage? (scale of 1-10)"
 
 squeak = gets.chomp
   squeak = squeak.to_i
 
+p "#{squeak} is just the perfect amount of squeakage!"
+
 puts "What is the color of the hamster's fur?"
 
 color = gets.chomp
 
+p "Well #{color} just adds to the colorful world we live in!"
 
 puts "Is the hamster worthy of adoption?"
 
@@ -20,6 +24,7 @@ adopt = gets.chomp
     obj.to_s == "true"
   end
 
+p "Okay, #{adopt}. We will keep that in mind!"
 
 puts "What is the estimate age of the hamster?"
 
@@ -29,3 +34,5 @@ age = gets.strip
   else
     age = age.to_i
   end
+
+p "#{age}! I feel wisdom radiating from this ham!"

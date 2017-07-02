1b8cb52bbff42f7627c8c17398d74a5ea167a441
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-01 20:02:58 -0500

    Update vapmpires questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,17 @@
+puts "What is your name?"
+
+name = gets.chomp
+
+puts "How old are you? What year were you born?"
+
+age = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+
+bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+
+health = gets.chomp
+
+end

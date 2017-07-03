f8ee29432e8d12b06f9b78ebd4d35b828187272f
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-03 10:58:42 -0500

    Update vampires questionnaire with allergies loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -5,7 +5,7 @@ counter = 0
     x = x.to_i
 
 while counter < x
-  puts counter
+
 
 
 puts "What is your name?"
@@ -50,13 +50,31 @@ health = gets.chomp
     vamps_donut_need_insurance = false
   end
 
+
+loop_counter = 0
+
+loop do
+    puts "Please list any allergies (type DONE when done)"
+    allergy = gets.chomp
+    if allergy == "DONE"
+        puts "Thank you for listing your allergies"
+        break
+    elsif allergy == "sunshine"
+      vamps_hate_sun = true
+    else
+      vamps_hate_sun = false
+    end
+    loop_counter += 1
+end
+
+
 if !(vamps_are_old && vamps_hate_garlic && vamps_donut_need_insurance)
   puts "Probably not a vampire"
-elsif vamps_are_old && vamps_hate_garlic || vamps_donut_need_insurance
+elsif vamps_are_old && vamps_hate_garlic && vamps_hate_sun || vamps_donut_need_insurance
   puts "Probably a vampire"
-elsif vamps_are_old && vamps_hate_garlic && vamps_donut_need_insurance
+elsif vamps_are_old && vamps_hate_garlic && vamps_donut_need_insurance && vamps_hate_sun
   puts "Almost certainly a vampire"
-elsif vamp_names_are_obvi
+elsif vamp_names_are_obvi && vamps_hate_sun
   puts "Definitely a vampire"
 else
   puts "Results inconclusive"
@@ -64,3 +82,5 @@ end
 
   counter += 1
 end
+
+puts "Actually never mind! What do these questions have to do with anything?! Let's all be friends!"

9471f4c702e29e728b7ee230fac7e4541302cdcb
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-06 20:16:05 -0500

    Update method list with methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,40 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+puts "zom".insert(1, 'o')
 # => “zoom”
 
-# "enhance".<???>
+puts "enhance".center(20)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+x = "the usual"
+x << " suspects"
+puts x.concat(33)
+# => "the usual suspects!"
 
-# " suspects".<???>
+puts " suspects".insert(0, "the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+puts "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+y= "The mystery of the missing first letter"
+puts y[1..38]
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+puts "Elementary,    my   dear        Watson!".split.join(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
+puts "z".ord
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
+# (122 is the ASCII [American Standard Code for Information Interchange] marker for "z")
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts "How many times does the letter 'a' appear in this string?".count("a")
 # => 4

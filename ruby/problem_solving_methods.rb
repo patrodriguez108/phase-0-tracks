915914347e47e24434fc39880ba65a2760501172
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-09 19:05:26 -0500

    Update problem solving methods with search

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,15 @@
+def search_arr(var1, var2)
+  index = 0
+  none = nil
+  var1.each do |list|
+    if list == var2
+      none = index
+    else
+      index += 1
+    end
+  end
+  none
+end
+
+array = [23, 42, 6, 98, 100]
+p search_arr(array, 42)

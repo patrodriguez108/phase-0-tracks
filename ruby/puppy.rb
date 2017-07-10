be080d8f6dc9f414a7e6f3a96437aea7826d6540
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-10 16:29:57 -0500

    Update puppy file with class design

---
+++ ruby/puppy.rb
@@ -0,0 +1,12 @@
+# Species
+# Pancakus Biscuitus
+
+# Characteristics
+# Goodboy: yes
+# Fetch: yes
+# Likes vaccum: no
+
+# Behavior
+# Runs in circles in the park
+# Hates sirens
+# Loves everything

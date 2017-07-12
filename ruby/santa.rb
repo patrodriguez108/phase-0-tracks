cb32f18e3b499b7793eeac0a741d75962428d2ac
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-12 16:53:25 -0500

    Update file with greeting instance and speak method

---
+++ ruby/santa.rb
@@ -0,0 +1,29 @@
+# define santa class
+
+class Santa
+end
+
+# create instance of santa class for speak
+# define method
+# print "Ho ho ho! Haaaaapy Holidays!"
+# end method
+
+greeting = Santa.new
+  def speak
+    puts "Ho ho ho! Haaaaapy Holidays!"
+  end
+
+# print speak method
+
+puts speak
+
+# write out method scope for eat_milk_and_cookies method
+# print "That was a good <parameter>!"
+# end method
+
+# print eat_milk_and_cookies method
+
+# write out method scope for initialize method
+# print "Initializing Santa instance..."
+
+# print initialize method

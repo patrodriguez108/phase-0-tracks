e599e7b63751f36706e2307d5072941506c3f395
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-13 18:53:19 -0500

    Add list file; deleted todo_list file

---
+++ ruby/list/list.rb
@@ -0,0 +1,18 @@
+class TodoList
+
+  def get_items(x, y)
+    list = [x, y]
+  end
+
+  # def add_item(z)
+  #    list.push(z)
+  # end
+
+  # def delete_item(x)
+  #   list.delete(x)
+  # end
+
+  # def get_item(x)
+
+
+end

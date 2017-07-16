a0693ed1791fbce860bf4e38ec48344d33a4e1e8
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-16 01:13:29 -0500

    Update list by uncommenting other methods and updating 4th method

--- ruby/list/list.rb
+++ ruby/list/list.rb
@@ -1,23 +1,24 @@
 class TodoList
 
   def initialize(x, y)
-    list = [x, y]
-    p list
+    @list = [x, y]
   end
 
-  def get_items(list)
-    list
+  def get_items
+    @list
   end
 
-  # def add_item(z)
-  #    list.push(z)
-  # end
+  def add_item(z)
+    @list.push(z)
+  end
 
-  # def delete_item(x)
-  #   list.delete(x)
-  # end
+  def delete_item(x)
+    @list.delete(x)
+  end
 
-  # def get_item(x)
-
+  def get_item(i)
+    i.to_i
+    p @list.index[i]
+  end
 
 end

e9050c93198e6e327095d45141047da8c4b7b253
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-16 19:28:32 -0500

    Add game file with pseudocode

---
+++ ruby/solo-game/game.rb
@@ -0,0 +1,50 @@
+# game class
+
+# make a class for the game
+
+#   define initialize method
+#     - attribute for empty array
+#     - attribute for guess count
+#     - attribute for game over
+#   end
+
+#   define method that asks user1 for a word
+#     - attribute for the word
+#     - print the word
+#   end
+
+#   define method that splits word into array
+#     - split word into array
+#       - iterate through array
+#       - push into blank array
+#     - have guess count equal to amount of characters in array
+#     - display blank word to user2
+#   end
+
+#   define method that searches array by letter
+#     - have guess count go up by one
+#     - enter guess limit
+#     - if user2 guesses a correct letter
+#       - correct letter fills in the blank
+#     - elsif user2 guesses incorrect letter
+#       - blanks remain blank
+#     - end
+#   end
+
+# end
+
+# driver code
+
+# greet users
+# make new instance
+
+# ask user1 to enter a word (call on word method)
+# user input for word
+
+# ask user2 to guess the word
+# user input for letters
+# if user2 right
+#   encouraging message
+# else user2 wrong
+#   taunting message
+# end

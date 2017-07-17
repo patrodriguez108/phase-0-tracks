2d5dea2f74201de95724abe167ceedbeec8dfaee
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-17 02:46:29 -0500

    Update game file commenting out instance methods that aren't green

--- ruby/solo-game/game.rb
+++ ruby/solo-game/game.rb
@@ -1,26 +1,24 @@
 class WordGame
-  # attr_reader :guess_count
-  # attr_reader :is_over
-  # attr_reader :word_letters
-  # attr_reader :blank_arr
+  attr_reader :guess_count
+  attr_reader :is_over
+  attr_reader :word_letters
+  attr_reader :blank_arr
+  attr_reader :word
 
-  # def initialize
-  #   @word_letters = []
-  #   @guess_count = 0
-  #   @is_over = false
-  # end
+  def initialize
+    @word_letters = []
+    @guess_count = 0
+    @is_over = false
+  end
 
   def enter(word)
     @word = word
-    p word
+    lets = @word.split('')
+    lets.each do |search|
+      @word_letters << search
+    end
+    p @word_letters
   end
-
-  # def divide
-  #   lets = @word.split('')
-  #   lets.each do |search|
-  #     @word_letters << search
-  #   end
-  # end
 
   # def blank_arr
   #   @blank_arr = @word_letters.clone
@@ -38,12 +36,14 @@ class WordGame
   #     @is_over = true
   #   end
   #   if @word_letters.include?(letter)
-  #     puts "You got a letter"
+  #     p "You got a letter"
 
   #   else
-  #     puts "Sorry, guess again"
+  #     p "Sorry, guess again"
 
   #   end
   # end
 
 end
+
+# It's almost 3 AM and I need to wake up early for work. I decided to submit what I have. If I have time I do want to go back and redo this.

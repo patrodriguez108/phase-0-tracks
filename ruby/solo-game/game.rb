2deee32fdf3568c332664d553d1d253d1bdf3050
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-17 02:20:32 -0500

    Update game file by commenting out all methods but one

--- ruby/solo-game/game.rb
+++ ruby/solo-game/game.rb
@@ -1,45 +1,49 @@
 class WordGame
-  attr_reader :guess_count
-  attr_reader :is_over
-  attr_reader :word_letters
-  attr_reader :blank_arr
+  # attr_reader :guess_count
+  # attr_reader :is_over
+  # attr_reader :word_letters
+  # attr_reader :blank_arr
 
-  def initialize
-    @word_letters = []
-    @guess_count = 0
-    @is_over = false
-  end
+  # def initialize
+  #   @word_letters = []
+  #   @guess_count = 0
+  #   @is_over = false
+  # end
 
   def enter(word)
     @word = word
-    lets = @word.split('')
-    lets.each do |search|
-      @word_letters << search
-    end
+    p word
   end
 
-  def blank_arr
-    @blank_arr = @word_letters.clone
-    @blank_arr.each do |this|
-      this.replace('_')
-    end
-    status = @blank_arr.join(' ')
-    @status = status
-    p status
-  end
+  # def divide
+  #   lets = @word.split('')
+  #   lets.each do |search|
+  #     @word_letters << search
+  #   end
+  # end
 
-  def search(letter)
-    @guess_count += 1
-    if @guess_count == @word_letters.count
-      @is_over = true
-    end
-    if @word_letters.include?(letter)
-      puts "You got a letter"
+  # def blank_arr
+  #   @blank_arr = @word_letters.clone
+  #   @blank_arr.each do |this|
+  #     this.replace('_')
+  #   end
+  #   status = @blank_arr.join(' ')
+  #   @status = status
+  #   p status
+  # end
 
-    else
-      puts "Sorry, guess again"
+  # def search(letter)
+  #   @guess_count += 1
+  #   if @guess_count == @word_letters.count
+  #     @is_over = true
+  #   end
+  #   if @word_letters.include?(letter)
+  #     puts "You got a letter"
 
-    end
-  end
+  #   else
+  #     puts "Sorry, guess again"
+
+  #   end
+  # end
 
 end

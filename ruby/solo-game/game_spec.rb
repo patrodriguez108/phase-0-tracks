923a54bb8f763195f5acb7c8743bb0e6a354af0c
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-17 02:20:53 -0500

    Update spec file commenting out all but one test

--- ruby/solo-game/game_spec.rb
+++ ruby/solo-game/game_spec.rb
@@ -4,16 +4,29 @@ describe WordGame do
   let(:game) { WordGame.new }
 
   it "enters word" do
-    expect(game.enter("example")) to eq ["e", "x", "a", "m", "p", "l", "e"]
+    game.enter("example")
+    expect(game.enter).to eq "example"
   end
 
-  it "creates word blank" do
-    expect(game.blank_arr) to eq ("_ _ _ _ _ _ _")
-  end
+end
 
-  it "searches letter" do
-    game.search("m") to eq ("You got a letter")
-    game.search("z") to eq ("Sorry, guess again")
-  end
+
+# require_relative 'game'
+
+# describe WordGame do
+#   let(:game) { WordGame.new }
+
+#   it "enters word" do
+#     expect(game.enter("example") to eq ("example")
+#   end
+
+  # it "creates word blank" do
+  #   expect(game.blank_arr) to eq ("_ _ _ _ _ _ _")
+  # end
+
+  # it "searches letter" do
+  #   game.search("m") to eq ("You got a letter")
+  #   game.search("z") to eq ("Sorry, guess again")
+  # end
 
 end

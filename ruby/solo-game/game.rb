025b5074a86b640209596eb31f4e0e3c3c1386bf
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-16 20:43:40 -0500

    Update game file with new data

--- ruby/solo-game/game.rb
+++ ruby/solo-game/game.rb
@@ -1,3 +1,37 @@
+class WordGame
+
+  def initialize
+    @word_letters = []
+    @guess_count = 0
+    @game_over = false
+  end
+
+  def enter(word)
+    @word = word
+  end
+
+  def divide
+    lets = @word.split('')
+    lets.each do |search|
+      @word_letters << search
+    end
+  end
+
+
+  def search(letter)
+    if @word_letters.include?(letter)
+      puts "You got a letter"
+    else
+      puts "Sorry, guess again"
+    end
+    @guess_count += 1
+    if @guess_count == @word_letters.count
+      @game_over = true
+    end
+  end
+
+end
+
 # game class
 
 # make a class for the game
@@ -10,7 +44,6 @@
 
 #   define method that asks user1 for a word
 #     - attribute for the word
-#     - print the word
 #   end
 
 #   define method that splits word into array
@@ -32,6 +65,19 @@
 #   end
 
 # end
+
+puts "Hello, welcome to the Game of Words"
+puts "When you play the Game of Words you win or you die!!!!!"
+game = WordGame.new
+
+puts "Player 1, please enter a word"
+entry = gets.chomp
+game.enter(entry)
+game.divide
+
+puts "Okay Player 2, guess which letters are in the word one by one!"
+character = gets.chomp
+game.search(character)
 
 # driver code
 

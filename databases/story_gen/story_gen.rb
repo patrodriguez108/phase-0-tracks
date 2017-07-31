4af1273df7df78187849308ebd7e6f61d3c0bc66
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-07-31 01:40:23 -0500

    Add story gen file with data

---
+++ databases/story_gen/story_gen.rb
@@ -0,0 +1,42 @@
+# require gems
+require 'sqlite3'
+require 'faker'
+
+# create SQLite3 database
+db = SQLite3::Database.new("tropes.db")
+
+# # add string delimiter
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS story(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    setting VARCHAR(255),
+    conflict VARCHAR(255)
+  )
+SQL
+
+# create table
+db.execute(create_table_cmd)
+
+# add a test story
+# db.execute("INSERT INTO story (name, setting, conflict) VALUES ('Boris', 'A Long Time Ago, in a Galaxy Far Far Away...', 'Government Conspiracy' )")
+
+# create arrays of settings and conflicts
+
+settings = ["Just Before the End", "Raygun Gothic", "Small Town Rivalry", "Ghost Town", "Company Town", "Boarding School of Horrors", "Planet of Hats", "The Shangri-La", "Feudal Future", "City Noir", "City of Adventure", "City of Spies", "Close-Knit Community", "Mirror Universe"]
+
+conflicts = ["Shapeshifter Identity Crisis", "Split-Personality Takeover", "Hope Is Scary", "What You Are in the Dark", "Double Think", "Race Against the Clock", "Rage Against the Heavens", "Random Events Plot", "No Plot? No Problem!", "Never Learned to Read"]
+
+def create_story(db, name, setting, conflict)
+  db.execute("INSERT INTO story (name, setting, conflict) VALUES (?, ?, ?)", [name, setting, conflict])
+end
+
+# 30.times do
+#   create_story(db, Faker::Name.name, settings.sample, conflicts.sample)
+# end
+
+# explore ORM by retrieving data
+stories = db.execute("SELECT * FROM story;")
+stories.each do |story|
+ puts "#{story['name']} is in the world of #{story['setting']}. The plot involves #{story['conflict']}"
+end

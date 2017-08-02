7503f61f85f1e6bbeb8112b80d85c1a098e89b00
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-08-02 14:18:06 -0500

    Add sherry java file

---
+++ web_dev/client_side_js/sherry.js
@@ -0,0 +1,9 @@
+var ital = document.getElementsByTagName("em");
+
+var el = ital[0];
+
+el.style.border = "3px solid red";
+
+var shades = document.getElementById("glasses");
+
+shades.hidden = true;
